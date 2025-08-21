afb760f1cb4e7f8f398e8c970753af71f93c928a
Dakota J. Keeler <HolyFenrir@protonmail.com>
2025-08-20 20:28:58 -0400

    updated to 0.15.1

--- build.zig
+++ build.zig
@@ -28,23 +28,4 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-
-    // static lib for c consumers
-    const lib = b.addStaticLibrary(.{
-        .name = "dklib",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib);
-
-    const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_tests = b.addRunArtifact(unit_tests);
-    b.step("test", "Run dklib tests").dependOn(&run_tests.step);
 }
